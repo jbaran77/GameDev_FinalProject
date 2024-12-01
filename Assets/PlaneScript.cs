2cccefdfeecbda7feab305b7491223b2a98cea02
John Baran <jbaran@villanova.edu>
2024-12-01 12:25:45 -0500

    environment, engine effects

--- Assets/PlaneScript.cs
+++ Assets/PlaneScript.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+
 
 public class PlaneScript : MonoBehaviour {
 
     Rigidbody rb;
+    AudioSource audioSource;
     
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
         
     }
+
+    [SerializeField] ParticleSystem engine1;
+
+    [SerializeField] ParticleSystem engine2;
     [SerializeField] float throttleIncrement = 1f;
-    [SerializeField] float maxThrust = 200f;
+    [SerializeField] float maxThrust = 400f;
     [SerializeField] float responsiveness = 3f;
     [SerializeField] float lift = 135f;
 
     [SerializeField] TextMeshProUGUI hud;
+    [SerializeField] TextMeshProUGUI altWarn;
 
     private float throttle;
     private float roll;
     private float pitch;
     private float yaw;
+
+    private float numHits = 0;
 
     
 
@@ -41,9 +52,18 @@ public class PlaneScript : MonoBehaviour {
 
 
         if (Input.GetKey(KeyCode.Space)) {
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+            engine1.Play();
+            engine2.Play();
+
             throttle += throttleIncrement;
-        } else if (Input.GetKey(KeyCode.LeftControl)) {
+        } else  {
+            audioSource.Stop();
             throttle -= throttleIncrement;
+            engine1.Stop();
+            engine2.Stop();
         }
 
         throttle = Mathf.Clamp(throttle, 0f, 100f);
@@ -53,7 +73,17 @@ public class PlaneScript : MonoBehaviour {
     private void Update() {
         HandleInputs();
         updateHud();
-        
+        CheckAltitude();
+    }
+
+    private void CheckAltitude() {
+
+
+         if (transform.position.y > 100) {
+            altWarn.text = "Too High";
+           
+            
+         }
     }
 
     private void FixedUpdate() {
@@ -70,6 +100,27 @@ public class PlaneScript : MonoBehaviour {
         hud.text = "Throttle: " + throttle.ToString("F0") + "%\n";
         hud.text += "Airspeed: " + (rb.velocity.magnitude *3.6f).ToString("F0") + " Km/h\n";
         hud.text += "Altitude: " + transform.position.y.ToString("F0") + " m";
+    }
+
+     private void OnCollisionEnter(Collision other) {
+        switch (other.gameObject.tag) {
+            case "Mountains":
+            numHits ++;
+            if(numHits == 3){
+                ReloadLevel();
+            }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void ReloadLevel() {
+        Scene sceneManager = SceneManager.GetActiveScene();
+        int currentIndex = sceneManager.buildIndex;
+        SceneManager.LoadScene(currentIndex);
+
     }
 }
 
